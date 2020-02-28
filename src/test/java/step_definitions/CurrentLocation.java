cdcf52e4062030dcb95e899ea5c2548f98f81a56
Anvar Sono <kerimbekovich89@gmail.com>
2020-02-27 22:04:05 -0600

    p

--- src/test/java/step_definitions/CurrentLocation.java
+++ src/test/java/step_definitions/CurrentLocation.java
@@ -3,6 +3,7 @@ package step_definitions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LandingPage;
 import pages.LoginPage;
@@ -12,39 +13,53 @@ import utilities.Driver;
 
 public class CurrentLocation {
     LoginPage loginPage = new LoginPage();
-    @Given("User is on Landing page")
-    public void user_is_on_Landing_page() {
-        Driver.getDriver().get(Config.getProperty("brUrl"));
-        RepairsModulePage repairsModulePage = new RepairsModulePage();
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+    LandingPage landingPage = new LandingPage();
+    RepairsModulePage repairsModulePage = new RepairsModulePage();
 
-        LandingPage landingPage = new LandingPage();
+
+
+    @Given("User is on Landing page")
+    public void user_is_on_Landing_page() throws InterruptedException {
+        Driver.getDriver().get(Config.getProperty("brUrl"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        loginPage.emailInput.sendKeys(Config.getProperty("briteMUsername"));
+        loginPage.passwordInput.sendKeys(Config.getProperty("briteMPassword"));
+        loginPage.logInButton.click();
+        Thread.sleep(5000);
+
 
     }
 
     @When("User click to repairs module")
-    public void user_click_to_repairs_module() {
-
-
+    public void user_click_to_repairs_module() throws InterruptedException {
+        landingPage.repairsModuleButton.click();
+        Thread.sleep(5000);
     }
 
     @When("User click create button")
     public void user_click_create_button() {
+        repairsModulePage.RepairOrderCreateButton.click();
+
+
 
     }
 
     @When("User click current location box")
     public void user_click_current_location_box() {
+        repairsModulePage.currentLocationBox.click();
 
     }
 
     @When("User select create and edit from dropdown list")
     public void user_select_create_and_edit_from_dropdown_list() {
+        repairsModulePage.currentLocationCreateAndEdit.click();
 
     }
 
     @Then("User sees pop-up page")
     public void user_sees_pop_up_page() {
+        Assert.assertTrue(repairsModulePage.currentLocationCreateAndEditPagePopUp.isDisplayed());
+
 
     }
 
--- target/cucumber-reports/report.js
+++ target/cucumber-reports/report.js
@@ -1,156 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/productToRepair.feature");
-formatter.feature({
-  "name": "Product to repair feature",
-  "description": "  Agile story: As a User I should be able to access repairs list, search for repair, create/edit/delete a repair",
-  "keyword": "Feature"
-});
-formatter.scenario({
-  "name": "",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@smokeTest4"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User is logged in to briteErp Login Page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_is_logged_in_to_briteErp_Login_Page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User clicks to Repair module on Landing Page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_to_Repair_module_on_Landing_Page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User click to Create button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_Create_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User click to ProductToRepair button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_ProductToRepair_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User click to SearchMore button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_SearchMore_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to see a list of products",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_see_a_list_of_products()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User click to Create/Edit button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_Create_Edit_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should see grey text \"Product name\"",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_see_grey_text(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to enter his name",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_enter_his_name()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to choose a ProductType",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_choose_a_ProductType()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to enter an InternalReference",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_enter_an_InternalReference()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to enter a Barecode",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_enter_a_Barecode()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should be able to choose a Category",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_be_able_to_choose_a_Category()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("src/test/resources/features/repairsModule.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/repairsModule.feature");
 formatter.feature({
   "name": "Repairs Module Verification",
   "description": "  Agile story: As a User I should be able to access repairs list, search for repair, create/edit/delete a repair",
@@ -164,62 +12,72 @@ formatter.background({
 formatter.before({
   "status": "passed"
 });
+formatter.step({
+  "name": "User is on Landing page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "CurrentLocation.user_is_on_Landing_page()"
+});
+formatter.result({
+  "status": "passed"
+});
 formatter.scenario({
-  "name": "Customer Dropdown Verification",
+  "name": "Current location \"Create and edit\" verification",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@smokeTest1"
+      "name": "@currentLocationCreateAndEdit"
     }
   ]
 });
 formatter.step({
-  "name": "User is logged in to briteErp Login Page",
-  "keyword": "Given "
+  "name": "User click to repairs module",
+  "keyword": "When "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_is_logged_in_to_briteErp_Login_Page()"
+  "location": "CurrentLocation.user_click_to_repairs_module()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks to Repair module on Landing Page",
+  "name": "User click create button",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_to_Repair_module_on_Landing_Page()"
+  "location": "CurrentLocation.user_click_create_button()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User click to Create button",
+  "name": "User click current location box",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_Create_button()"
+  "location": "CurrentLocation.user_click_current_location_box()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User types \"elif\" to Customer field",
+  "name": "User select create and edit from dropdown list",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_types_to_Customer_field(String)"
+  "location": "CurrentLocation.user_select_create_and_edit_from_dropdown_list()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User should see \"elif\" in the dropdown",
+  "name": "User sees pop-up page",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_should_see_in_the_dropdown(String)"
+  "location": "CurrentLocation.user_sees_pop_up_page()"
 });
 formatter.result({
   "status": "passed"
@@ -235,180 +93,79 @@ formatter.background({
 formatter.before({
   "status": "passed"
 });
+formatter.step({
+  "name": "User is on Landing page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "CurrentLocation.user_is_on_Landing_page()"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchSessionException: invalid session id\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027Anvars-MacBook-Pro.local\u0027, ip: \u0027fe80:0:0:0:83:14fe:b3f5:81c5%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15\u0027, java.version: \u002711.0.4\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.122, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: /var/folders/0r/rrs1rwd94h3...}, goog:chromeOptions: {debuggerAddress: localhost:63322}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 113969c63cf8ab7425a9091091c973f0\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:277)\n\tat step_definitions.CurrentLocation.user_is_on_Landing_page(CurrentLocation.java:23)\n\tat ✽.User is on Landing page(src/test/resources/features/repairsModule.feature:5)\n",
+  "status": "failed"
+});
 formatter.scenario({
-  "name": "Customer : Search header verification",
+  "name": "Current location \"Search more\" verification",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@smokeTest2"
+      "name": "@currentLocationSearchMore"
     }
   ]
 });
 formatter.step({
-  "name": "User is logged in to briteErp Login Page",
-  "keyword": "Given "
+  "name": "User click to repairs module",
+  "keyword": "When "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_is_logged_in_to_briteErp_Login_Page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User clicks to Repair module on Landing Page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_to_Repair_module_on_Landing_Page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User click to Create button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_Create_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User cliks to customer field",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_cliks_to_customer_field()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User cliks Search more field",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_cliks_Search_more_field()"
-});
-formatter.result({
-  "error_message": "java.lang.NullPointerException\n\tat step_definitions.CustomerField_StepDefs.user_cliks_Search_more_field(CustomerField_StepDefs.java:82)\n\tat ✽.User cliks Search more field(src/test/resources/features/repairsModule.feature:21)\n",
-  "status": "failed"
-});
-formatter.step({
-  "name": "User should see Search:customer header",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_should_see_Search_customer_header()"
+  "location": "CurrentLocation.user_click_to_repairs_module()"
 });
 formatter.result({
   "status": "skipped"
 });
-formatter.embedding("image/png", "embedded0.png");
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "Given User is on Landing page",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@smokeTest3"
-    }
-  ]
-});
 formatter.step({
-  "name": "User is logged in to briteErp Login Page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_is_logged_in_to_briteErp_Login_Page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User clicks to Repair module on Landing Page",
+  "name": "User click create button",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_to_Repair_module_on_Landing_Page()"
+  "location": "CurrentLocation.user_click_create_button()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "User click to Create button",
+  "name": "User click current location box",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_click_to_Create_button()"
+  "location": "CurrentLocation.user_click_current_location_box()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "User cliks to customer field",
+  "name": "User select search more from dropdown list",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_cliks_to_customer_field()"
+  "location": "CurrentLocationSearchMore.user_select_search_more_from_dropdown_list()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
-  "name": "User clicks create and edit field",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_create_and_edit_field()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User types elif in to name box",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_types_elif_in_to_name_box()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User clicks save button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CustomerField_StepDefs.user_clicks_save_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User should see elif in the customer field",
+  "name": "User see pop up page",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CustomerField_StepDefs.user_should_see_elif_in_the_customer_field()"
+  "location": "CurrentLocationSearchMore.user_see_pop_up_page()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchSessionException: invalid session id\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027Anvars-MacBook-Pro.local\u0027, ip: \u0027fe80:0:0:0:83:14fe:b3f5:81c5%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15\u0027, java.version: \u002711.0.4\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.122, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: /var/folders/0r/rrs1rwd94h3...}, goog:chromeOptions: {debuggerAddress: localhost:63322}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 113969c63cf8ab7425a9091091c973f0\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:295)\n\tat step_definitions.Hooks.tearDown(Hooks.java:30)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:566)\n\tat cucumber.runtime.Utils$1.call(Utils.java:26)\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\n\tat cucumber.runtime.Utils.invoke(Utils.java:20)\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\n\tat cucumber.runner.HookDefinitionMatch.runStep(HookDefinitionMatch.java:16)\n\tat cucumber.runner.TestStep.executeStep(TestStep.java:63)\n\tat cucumber.runner.TestStep.run(TestStep.java:49)\n\tat cucumber.runner.TestCase.run(TestCase.java:48)\n\tat cucumber.runner.Runner.runPickle(Runner.java:40)\n\tat cucumber.runtime.junit.PickleRunners$NoStepDescriptions.run(PickleRunners.java:146)\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:68)\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:23)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:73)\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:124)\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:65)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\n\tat cucumber.api.junit.Cucumber$1.evaluate(Cucumber.java:133)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\n\tat com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:47)\n\tat com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:242)\n\tat com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)\n",
+  "status": "failed"
 });
 });
