7ae6c05155ee95b9584c34444470e392107e1b10
Eduard Enache <enacheeduardc@gmail.com>
2020-02-28 18:39:02 -0600

    newx

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "step_definitions",
         dryRun = false,
-        tags = "@test2"   // we can pass more than one tag, ex: tags = "@smokeTest, @regressionTest"
+        tags = "@smoke"   // we can pass more than one tag, ex: tags = "@smokeTest, @regressionTest"
 )
 
 
